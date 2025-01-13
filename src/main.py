d00784efac87b878b7e837b991f68bdd9ae82cfe
Hichem-E <hichem.elmohri@gmail.com>
2025-01-12 23:49:22 -0500

    added recursive deletion and copy of files from static to public

--- src/main.py
+++ src/main.py
@@ -12,29 +12,6 @@ def main():
     if os.path.exists("public/"):
         shutil.rmtree("public")
     os.makedirs("public/", exist_ok=True)
-
-    def file_copy(directory):
-        walk = os.walk(directory)
-        for root, dirs, files in walk:
-            print("root is", root, "here are dirs", dirs, "here are files", files)
-            break
-
-
-    # for x in os.scandir('static'):
-    #     if x.is_dir():
-    #         print(x.name, "is a directory!")
-    #         new_dir = os.path.join('public/', x.name)
-    #         os.mkdir(new_dir)
-    #     if x.is_file():
-    #         print(x.name, "is a file!")
-            
-    #         shutil.copy('static/'+x.name, 'public/'+x.name)
-
-        
-
-            
-            
-    
     
     def rec_fn(dir, copy_dir):
         for x in os.scandir(dir):
@@ -48,8 +25,10 @@ def main():
                 rec_fn(next_s_dir, next_c_dir)
 
             elif x.is_file():
+                old_file_path = os.path.join(dir, x.name)
                 new_file_path = os.path.join(copy_dir, x.name)
-                shutil.copy(dir+x.name, copy_dir+x.name)
+                
+                shutil.copy(old_file_path, new_file_path)
 
     rec_fn("static", "public")
 
